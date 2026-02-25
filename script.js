7571e83e109bccc7b1d15fe43b342dd976a17e85
ub-victor <victoireushindi371@gmail.com>
2026-02-25 19:46:41 +0200

    University-web

---
+++ script.js
@@ -0,0 +1,24 @@
+function calculate() {
+
+    let cw = parseFloat(document.getElementById("cw").value) || 0;
+    let mid = parseFloat(document.getElementById("mid").value) || 0;
+    let exam = parseFloat(document.getElementById("exam").value) || 0;
+
+    let total = cw + mid + exam;
+    let average = total / 3;
+    let grade;
+
+    if (total >= 70) {
+        grade = "A";
+    } else if (total >= 60) {
+        grade = "B";
+    } else if (total >= 50) {
+        grade = "C";
+    } else {
+        grade = "Failed";
+    }
+
+    document.getElementById("total").value = total;
+    document.getElementById("average").value = average.toFixed(2);
+    document.getElementById("grade").value = grade;
+}
